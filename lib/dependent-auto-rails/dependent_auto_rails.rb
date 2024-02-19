e8f3c757dc2c67d62ce3e5776d5eb6a00e98d8a7
Joshua Young <djry1999@gmail.com>
2024-02-19 23:30:31 +1000

    Remove no longer needed validation bypassing monkey patches

--- lib/dependent-auto-rails/dependent_auto_rails.rb
+++ lib/dependent-auto-rails/dependent_auto_rails.rb
@@ -1,12 +1,8 @@
 # frozen_string_literal: true
 
 require "active_record"
-require "active_support"
 
 ActiveSupport.on_load(:active_record) do
-  require "dependent-auto-rails/activerecord/associations/builder/belongs_to"
-  require "dependent-auto-rails/activerecord/associations/builder/has_one"
-  require "dependent-auto-rails/activerecord/associations/builder/has_many"
   require "dependent-auto-rails/activerecord/associations/builder/association"
 end
 
